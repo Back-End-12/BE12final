5cad4279bf173dbaf1b91b6b6d6461eae5dae669
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-12-01 15:40:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 // require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
